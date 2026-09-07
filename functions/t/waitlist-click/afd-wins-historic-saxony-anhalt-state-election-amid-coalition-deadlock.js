f33a972efd2c730d92abb2c6de60a5b5096ca69e
Charlie Ehlen <charlie.ehlen@ccc.ox.ac.uk>
2026-09-07 18:37:59 +0100

    Publish article pages

---
+++ functions/t/waitlist-click/afd-wins-historic-saxony-anhalt-state-election-amid-coalition-deadlock.js
@@ -0,0 +1,9 @@
+export async function onRequest() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Cache-Control": "no-store",
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
+}
